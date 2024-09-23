c73a8bc9e3af468f008edb9771d81c4845bfe405
sutej <sutejym122@gmail.com>
2024-09-23 16:02:23 -0400

    Initial Commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Sutej Ym on 19/08/2024.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
